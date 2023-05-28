023de79ee8317bd492773f40185dc8ef42701e2a
Yns-Sl <younes.slimani70@gmail.com>
2023-05-28 18:46:25 +0100

    push

---
+++ philo_bonus/includes/philo_bonus.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_bonus.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/28 18:44:38 by yes-slim          #+#    #+#             */
+/*   Updated: 2023/05/28 18:45:50 by yes-slim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_BONUS_H
+# define PHILO_BONUS_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include <sys/time.h>
+# include <pthread.h>
+# include <semaphore.h>
+
+/*--------------------------------*/
+//LIBFT
+/*--------------------------------*/
+
+//HELPERS
+/*--------------------------------*/
+
+#endif
