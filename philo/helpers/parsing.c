bb82118cb129b7aed8b5856205d6856e8afb4a4d
Younes Es-Slimani <yes-slim@e2r5p9.1337.ma>
2023-06-15 15:24:57 +0100

    last update

--- philo/helpers/parsing.c
+++ philo/helpers/parsing.c
@@ -6,11 +6,45 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 21:25:49 by yes-slim          #+#    #+#             */
-/*   Updated: 2023/06/03 16:02:13 by yes-slim         ###   ########.fr       */
+/*   Updated: 2023/06/15 15:23:37 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	int		si;
+	long	res;
+
+	i = 0;
+	res = 0;
+	si = 1;
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			si *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		if (res > INT_MAX)
+			return (-1);
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return ((int)res * si);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
 
 int	check_isdigit(char *av)
 {
@@ -35,9 +69,6 @@ int check_val(char **av)
 	i = 1;
 	if (ft_atoi(av[i++]) < 1)
 		return (0);
-	while (av[i])
-		if (ft_atoi(av[i++]) < 60)
-			return (0);
 	return (1);
 }
 
--- philo/philo.c
+++ philo/philo.c
@@ -6,33 +6,19 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 18:34:15 by yes-slim          #+#    #+#             */
-/*   Updated: 2023/06/12 22:31:09 by yes-slim         ###   ########.fr       */
+/*   Updated: 2023/06/15 15:18:14 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int get_time(int *start, int stat);
-
 int	main(int ac, char **av)
-{
-	int start;
-	
+{	
 	if (ac == 5 || ac == 6)
 	{
 		if (!parsing(av))
 			return (0);
 	}
-	get_time(&start, 0);
 	return (0);
 }
 
-int get_time(int *start, int stat)
-{
-	struct timeval	time;
-	gettimeofday(&time, NULL);
-	
-	if (stat == 0)
-		return (*start = time.tv_usec, 0);
-	return (time.tv_usec - *start);
-}
